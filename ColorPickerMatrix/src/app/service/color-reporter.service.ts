9b3b502f78974efa2ccccb2ac78b112a6773512c
Sdb <sdebellis@gmail.com>
2023-11-28 00:28:48 +0000

    Add flask with routes. Add logout task to frontend.

--- ColorPickerMatrix/src/app/service/color-reporter.service.ts
+++ ColorPickerMatrix/src/app/service/color-reporter.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Subject } from 'rxjs';
+import { BehaviorSubject, Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -8,6 +8,8 @@ import { Subject } from 'rxjs';
 export class ColorReporterService {
   private colorSubject = new Subject<{ color: string; row: number; col: number }>;
   color$ = this.colorSubject.asObservable();
+  private logoutSubject = new BehaviorSubject<boolean>(false);
+  logout$ = this.logoutSubject.asObservable();
 
   constructor(private http: HttpClient) {}
 
@@ -20,8 +22,20 @@ export class ColorReporterService {
   }
 
   private sendColorToAPI(data: { color: string, row: number, col: number }) {
-    const apiUrl = 'https://myapi.org/picker-change';
+    const apiUrl = 'https://localhost/picker-change';
     return this.http.post(apiUrl, data);
+  }
+
+  logout() {
+    this.sendLogout().subscribe(
+      response => console.log("logged out of matrix."),
+      error => console.error("Error sending logout.")
+    );
+  }
+
+  private sendLogout() {
+    const apiUrl = 'https://localhost/logout';
+    return this.http.post(apiUrl, { "logout": true });
   }
 }
  
