38e138841e4fef831f4a9d5196fb072ddd86ce86
Sean DeBellis <sdebellis@gmail.com>
2023-12-27 14:45:12 -0800

    Update matrix usage.

--- matrix.py
+++ matrix.py
@@ -95,9 +95,16 @@ class NeopixelMatrix:
         self.rows = rows
         self.cols = cols
         self.auto_write = auto_write
-        self.pixels = neopixel.NeoPixel(
-            pixel_pin, rows * cols, brightness = 0.1, auto_write=auto_write
-        )
+        self.pixel_pin = pixel_pin
+        self.pixels = None
+
+    def initialize(self, rows, cols) -> None:
+        if not self.pixels:
+            self.rows = rows
+            self.cols = cols
+            self.pixels = neopixel.NeoPixel(
+                self.pixel_pin, rows * cols, brightness = 0.1, auto_write=self.auto_write
+                )
 
     def color_from_hex(self, hex_str) -> (int, int, int):
         h = hex_str.lstrip('#')
@@ -115,38 +122,44 @@ class NeopixelMatrix:
         return (row * self.cols) + (self.cols - col - 1)
 
     def change_pixel_color(self, color, row, col) -> None:
-        self.pixels[self.get_pixel_location(row, col)] = color
-        if not self.auto_write:
-            self.pixels.show()
-
-    def change_pixel_colors(self, pixel_data_json) -> None:
-        pixel_data_map = json.loads(pixel_data_json)
-        if 'data' in pixel_data_map:
-            for pixel in pixel_data_map['data']:
-                self.pixels[self.get_pixel_location(pixel['row'], pixel['col'])] = self.color_from_hex(pixel['color'])
+        if self.pixels:
+            self.pixels[self.get_pixel_location(row, col)] = color
             if not self.auto_write:
                 self.pixels.show()
 
+    def change_pixel_colors(self, pixel_data_json) -> None:
+        if self.pixels:
+            pixel_data_map = json.loads(pixel_data_json)
+            if 'data' in pixel_data_map:
+                for pixel in pixel_data_map['data']:
+                    self.pixels[self.get_pixel_location(pixel['row'], pixel['col'])] = self.color_from_hex(pixel['color'])
+                if not self.auto_write:
+                    self.pixels.show()
+
     def matrix_fill_rgb(self, r, g, b) -> None:
-        for i in range(self.rows):
-            for j in range(self.cols):
-                self.pixels[self.get_pixel_location(i,j)] = (r, g, b)
-        if not self.auto_write:
-            self.pixels.show()
+        if self.pixels:
+            for i in range(self.rows):
+                for j in range(self.cols):
+                    self.pixels[self.get_pixel_location(i,j)] = (r, g, b)
+            if not self.auto_write:
+                self.pixels.show()
 
     def matrix_fill_color(self, color) -> None:
         self.matrix_fill_rgb(color[0], color[1], color[2])
 
     def apply_mask(self, mask_matrix, mask_rows, mask_cols):
-        for i in range(mask_rows):
-            for j in range(mask_cols):
-                self.pixels[self.get_pixel_location(i, j)] = mask_matrix[i][j]
+        if self.pixels:
+            for i in range(mask_rows):
+                for j in range(mask_cols):
+                    self.pixels[self.get_pixel_location(i, j)] = mask_matrix[i][j]
 
-        if not self.auto_write:
-            self.pixels.show()
+            if not self.auto_write:
+                self.pixels.show()
 
     def deinit(self):
-        self.pixels.deinit()
+        if self.pixels:
+            self.pixels.deinit()
+            self.pixels = None
 
 def get_random_color():
     return (random.randint(0,255), random.randint(0,255), random.randint(0,255))
@@ -252,15 +265,15 @@ if __name__ == '__main__':
     app.secret_key = 'BAD_SECRET_KEY'
 
     cors = CORS(app, resources={r"/picker-change|/logout|/connect": {"origins": "http://localhost:port"}})
-    
+    pixel_matrix = None
 
     @app.route('/picker-change', methods = ['POST'])
     @cross_origin(origin='localhost',headers=['Content-Type','Authorization'])
     def picker_change(origin='localhost',headers=['Content-Type','Authorization']):
-        if 'pixel_matrix' in session:
+        if pixel_matrix:
             if request.content_type == 'application/json':
                 print(f"request.json type: {request.json}")
-                session['pixel_matrix'].change_pixel_colors(json.dumps(request.json))
+                pixel_matrix.change_pixel_colors(json.dumps(request.json))
                 return json.dumps({ "status": 200, "statusText": "OK", "connection-id": session['current_connection_uuid']})
 
             else:
@@ -280,7 +293,8 @@ if __name__ == '__main__':
                 data = request.get_json()
                 try:
                     session['current_connection_uuid'] = data['connection-id']
-                    session['pixel_matrix'] = NeopixelMatrix(ROWS, COLS, PIXEL_PIN, False)
+                    pixel_matrix = NeopixelMatrix(ROWS, COLS, PIXEL_PIN, False)
+                    pixel_matrix.initialize(ROWS,COLS)
                     return jsonify({"connection-id", session['current_connection_uuid']}), 200
                 except KeyError as ke:
                     return jsonify({"error": "Missing connection-id"}), 411
@@ -290,11 +304,11 @@ if __name__ == '__main__':
     @app.route('/logout')
     @cross_origin(origin='localhost',headers=['Content-Type','Authorization'])
     def logout():
-        if 'pixel_matrix' in session:
+        if pixel_matrix:
             try:
-                session['pixel_matrix'].deinit() # maybe return a server error code if this is problematic. 
+                pixel_matrix.deinit() # maybe return a server error code if this is problematic. 
             finally:
-                session.pop('pixel_matrix', default=None)
+                pixel_matrix = None
                 conn_id = session.pop('current_connection_uuid', default="")
                 return jsonify({"connection-id": conn_id}), 200
 
